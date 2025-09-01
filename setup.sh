c053f8eeed591076176391f025d5a857ba92e720
Supriya Thakur <supriyathakur744@gmail.com>
2025-09-01 19:22:03 +0530

    Updated files for deployment

---
+++ setup.sh
@@ -0,0 +1,14 @@
+﻿mkdir -p ~/.streamlit/
+
+echo "[server]
+headless = true
+enableCORS = false
+port = \
+
+[theme]
+primaryColor = '#1976D2'
+backgroundColor = '#FFFFFF'
+secondaryBackgroundColor = '#F0F2F6'
+textColor = '#000000'
+font = 'sans serif'
+" > ~/.streamlit/config.toml
